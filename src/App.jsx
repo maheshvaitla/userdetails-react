38757d90008bdc268c62029231f73cf5801498a7
maheshvaitla <maheshvaitla@gmail.com>
2021-12-30 09:27:57 +0530

    added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Userdetails } from './components/userdetails';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Userdetails />
+    </div>
+  );
+}
+
+export default App;
